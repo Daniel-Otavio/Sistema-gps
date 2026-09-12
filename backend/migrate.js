2ff368e43c31d6c3cb395480d1f9f2e0f00e1c78
Daniel-Otavio <67666186+Daniel-Otavio@users.noreply.github.com>
2026-09-12 09:14:51 -0300

    Delete backend/migrate.js

--- backend/migrate.js
+++
@@ -1,9 +0,0 @@
-const { executarMigracoes, pool } = require('../server');
-
-executarMigracoes()
-    .then(() => pool.end())
-    .catch(async erro => {
-        console.error('❌ Migração interrompida:', erro.message);
-        await pool.end().catch(() => {});
-        process.exitCode = 1;
-    });
